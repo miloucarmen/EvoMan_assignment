51c9277fa76c451da98ad44a0e875bd40f354723
miloucarmen <36204209+miloucarmen@users.noreply.github.com>
2019-09-17 14:10:21 +0200

    Evoman framework

---
+++ my_optimization.py
@@ -0,0 +1,30 @@
+################################
+# EvoMan FrameWork - V1.0 2016 #
+# Author: Karine Miras         #
+# karine.smiras@gmail.com      #
+################################
+
+# imports framework
+import sys, os
+sys.path.insert(0, 'evoman') 
+from environment import Environment
+from demo_controller import player_controller
+
+experiment_name = 'dummy_demo'
+if not os.path.exists(experiment_name):
+    os.makedirs(experiment_name)
+
+# initializes environment with ai player using random controller, playing against static enemy
+env = Environment(experiment_name=experiment_name,
+                  enemies=[2],
+                  playermode="ai",
+                  player_controller=player_controller(),
+                  enemymode="static",
+                  level=2,
+                  speed="fastest")
+env.play()
+
+np.random.seed(1)
+npop = 5
+generations = 10
+n_hidden = 10
