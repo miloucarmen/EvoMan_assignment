55e4001e32407be64ffeedc1cc7bad41b6bfd327
Bart van Laatum <hbpvanlaatum123@gmail.com>
2019-10-01 14:06:24 +0200

    started blend crossover

--- dummy_Milou.py
+++ dummy_Milou.py
@@ -23,10 +23,11 @@ if not os.path.exists(experiment_name):
 env = Environment(experiment_name=experiment_name)
 
 n_hidden = 10
-n_pop = 6
-n_weights = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5 
-max_gens = 3
+n_pop = 50
+n_weights = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5
+max_gens = 100
 n_gen = 0
+
 
 creator.create("FitnessMax", base.Fitness, weights = (1.0,))
 creator.create("Individual", list, fitness = creator.FitnessMax)
@@ -36,9 +37,7 @@ tlbx.register("atrr_float", random.random)
 tlbx.register("individual", tools.initRepeat, creator.Individual, tlbx.atrr_float, n = n_weights)
 tlbx.register("Population", tools.initRepeat, list, tlbx.individual, n = n_pop)
 
-log = tools.Logbook()
 Pop = tlbx.Population()
-best = tlbx.individual()
 
 
 # evaluation
@@ -47,24 +46,23 @@ def EvaluateFit(individual):
     return f,
 
 def Normalise(fit, fitnesses):
-    
+
     if ( max(fitnesses) - min(fitnesses) ) > 0 :
         fitnorm = ( fit - min(fitnesses) )/( max(fitnesses) - min(fitnesses) )
 
     else :
         fitnorm = 0
-    
+
     if fitnorm < 0:
             fitnorm = 0.0000000001
     return fitnorm
-    
-    
+
+
 
 tlbx.register("evaluate", EvaluateFit)
 tlbx.register("mate", tools.cxTwoPoint)
 tlbx.register('mutate', tools.mutFlipBit, indpb = 0.05)
 tlbx.register("select",tools.selTournament, tournsize = 3)
-tlbx.register('survival',tools.selTournament, tournsize = 3 )
 
 OffProb, MuProb = 0.5, 0.1
 # tlbx.register("normialise", Normalise)
@@ -76,58 +74,43 @@ fitns = list(map(tlbx.evaluate, Pop))
 # print(fitnsnorm)
 
 for ind, fit in zip(Pop, fitns):
-    print(fit)
     ind.fitness.values = fit
 
 fit = [ind.fitness.values[0] for ind in Pop]
 
-
 while max(fit) < 100 and n_gen < max_gens:
-
+    n_gen += 1
     print("---------------------Generation %i-------------------------", n_gen)
-    offspring = tlbx.select(Pop, len(Pop)//2)
+    offspring = tlbx.select(Pop, len(Pop))
     offspring = list(map(tlbx.clone, offspring))
-    originalpop = list(map(tlbx.clone, Pop))
 
     for child1, child2 in zip(offspring[::1], offspring[1::2]):
         if random.random() < OffProb:
-        
             tlbx.mate(child1,child2)
             del child1.fitness.values
             del child2.fitness.values
-        
+
     for mutant in offspring:
         if random.random() < MuProb:
-          
             tlbx.mutate(mutant)
             del mutant.fitness.values
 
-    new_ind = []
-    for ind in offspring:
-        if ind.fitness.valid != True:
-            new_ind.append(ind)
-    
-    # new_ind = [ind for ind in offspring if not ind.fitness.valid]
-    
+    new_ind = [ind for ind in offspring if not ind.fitness.valid]
     fitns = list(map(tlbx.evaluate, new_ind))
 
 
     for ind, fit in zip(new_ind, fitns):
         ind.fitness.values = fit
 
-    selectionpop = originalpop + new_ind
-    fitselect = [ind.fitness.values[0] for ind in selectionpop]
+    Pop[:] = offspring
 
-    Pop[:] = tlbx.survival(selectionpop, len(Pop))
-    
     fits = [ind.fitness.values[0] for ind in Pop]
+    length = len(Pop)
+    mean = sum(fits) / length
+    sum2 = sum(x*x for x in fits)
+    std = abs(sum2 / length - mean**2)**0.5
 
-    maxval = np.max(fits)
-    index = fits.index(maxval)
-    log.record(gen = n_gen, meanfit = np.mean(fits), varfit = np.var(fits), stdfit = np.std(fits), maxfit = maxval, optweightcombination = Pop[index])
-    n_gen += 1
-
-    if best.fitness.valid != True or best.fitness.values <= Pop[index].fitness.values:
-        best = tlbx.clone(Pop[index])
-
-print(best.fitness.values)
+    print("Min %s", min(fits))
+    print("Max %s", max(fits))
+    print("avg %s", mean)
+    print("Std %s", std)
--- dummy_bart.py
+++ dummy_bart.py
@@ -21,7 +21,22 @@ if not os.path.exists(experiment_name):
 env = Environment(experiment_name=experiment_name,
 				  enemies=[2])
 
+log = tools.Logbook()
+tlbx = base.Toolbox()
 env.state_to_log() # checks environment state
+
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", np.ndarray, fitness=creator.FitnessMax)
+
+tlbx.register("atrr_float", np.random.random)
+tlbx.register("individual", tools.initRepeat, creator.Individual, tlbx.atrr_float, n=n_weights)
+tlbx.register("population", tools.initRepeat, list, tlbx.individual, n=pop_size)
+tlbx.register("evaluate", evaluate)
+tlbx.register("mate", crossover)
+tlbx.register("mutate", self_adaptive_mutate, indpb=0.05)
+
+tlbx.register("select",tools.selTournament, tournsize = 3)
+tlbx.register('survival', natural_selection)
 random.seed(1)
 
 # standard variables
@@ -30,9 +45,9 @@ pop_size = 10
 n_gens = 20
 n_weights = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5
 
-# sigma for normal dist, tao constant,  mut_prob prop mutation for individu
+# sigma for normal dist, tau constant,  mut_prob prop mutation for individu
 sigma = 1
-tao = 1/np.sqrt(n_weights)
+tau = 1/np.sqrt(n_weights)
 mut_prob = 1/pop_size
 mate_prob = 0.7
 best_pop = (0, 0, 0)
@@ -43,8 +58,8 @@ def evaluate(x):
     return f,
 
 # changes sigma over time
-def modify_sigma(tao, sigma=sigma):
-    return sigma * np.exp(tao*np.random.normal(0,1))
+def modify_sigma(tau, sigma=sigma):
+    return sigma * np.exp(tau*np.random.normal(0,1))
 
 # mutates alles of gen with p indpb
 def self_adaptive_mutate(individual, sigma, indpb):
@@ -53,49 +68,23 @@ def self_adaptive_mutate(individual, sigma, indpb):
     xadd = np.where(np.random.random(normal_dist.shape) < 1-indpb, 0, normal_dist)
     return individual + xadd
 
+# natural selection of population, without replacement
 def natural_selection(selectionpop, pop_size):
     fitselect = [ind.fitness.values[0] for ind in selectionpop]
     pop = []
     for _ in range(pop_size):
         idx_inds = random.sample(range(len(fitselect)), 3)
         fitness_inds = np.array(fitselect)[idx_inds]
-        # fitness = [ind.fitness.values[0] for ind in inds]
         best_idx = idx_inds[np.argmax(fitness_inds)]
         pop.append(selectionpop.pop(best_idx))
         fitselect.pop(best_idx)
     return pop
 
-# normalises the fitness
-def Normalise(fit, fitnesses):
-
-    if ( max(fitnesses) - min(fitnesses) ) > 0 :
-        fitnorm = ( fit - min(fitnesses) )/( max(fitnesses) - min(fitnesses) )
-    else:
-        fitnorm = 0
-
-    if fitnorm < 0:
-            fitnorm = 0.0000000001
-    return fitnorm
+def blend_crossover(parent1, parent2):
+    di = parent1 - parent2
+    
 
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", np.ndarray, fitness=creator.FitnessMax)
-
-log = tools.Logbook()
-tlbx = base.Toolbox()
-
-tlbx.register("atrr_float", np.random.random)
-tlbx.register("individual", tools.initRepeat, creator.Individual, tlbx.atrr_float, n=n_weights)
-tlbx.register("population", tools.initRepeat, list, tlbx.individual, n=pop_size)
-
-tlbx.register("evaluate", evaluate)
-
-tlbx.register("mate", tools.cxUniform, indpb=0.1)
-tlbx.register("mutate", self_adaptive_mutate, indpb=0.05)
-
-# tlbx.register('mutate', tools.mutGaussian, mu=0, sigma=1, indpb = 0.05)
-tlbx.register("select",tools.selTournament, tournsize = 3)
-tlbx.register('survival', natural_selection)
 
 pop = tlbx.population()
 fitns = [tlbx.evaluate(individual) for individual in pop]
@@ -108,38 +97,38 @@ for n_gen in range(n_gens):
 
     offspring = tlbx.select(pop, len(pop)//2)
     offspring = list(map(tlbx.clone, offspring))
-    originalpop = list(map(tlbx.clone, pop))
+    # originalpop = list(map(tlbx.clone, pop))
 
-    sigma = modify_sigma(tao, sigma=sigma)
+    sigma = modify_sigma(tau, sigma=sigma)
 
     for parent1, parent2 in zip(offspring[::2], offspring[1::2]):
         if random.random() < mate_prob:
-
-            tlbx.mate(parent1,parent1)
-            del parent1.fitness.values
-            del parent2.fitness.values
-
-    for mutant in offspring:
-        if random.random() < mut_prob:
-
-            tlbx.mutate(mutant, sigma)
-            del mutant.fitness.values
-
-    new_ind = [ind for ind in offspring if ind.fitness.valid != True]
-    fitns = list(map(tlbx.evaluate, new_ind))
-
-    for ind, fit in zip(new_ind, fitns):
-        ind.fitness.values = fit
-
-    selectionpop = originalpop + new_ind
-    fitselect = [ind.fitness.values[0] for ind in selectionpop]
-    pop[:] = tlbx.survival(selectionpop, pop_size)
-
-    fits = np.array([ind.fitness.values for ind in pop])
-    print(fits)
-
+            for i in random.randint(0,4))
+                tlbx.mate(parent1,parent2)
+                del parent1.fitness.values
+                del parent2.fitness.values
+    #
+    # for mutant in offspring:
+    #     if random.random() < mut_prob:
+    #
+    #         tlbx.mutate(mutant, sigma)
+    #         del mutant.fitness.values
+    #
+    # new_ind = [ind for ind in offspring if ind.fitness.valid != True]
+    # fitns = list(map(tlbx.evaluate, new_ind))
+    #
+    # for ind, fit in zip(new_ind, fitns):
+    #     ind.fitness.values = fit
+    #
+    # selectionpop = originalpop + new_ind
+    # fitselect = [ind.fitness.values[0] for ind in selectionpop]
+    # pop[:] = tlbx.survival(selectionpop, pop_size)
+    #
+    # fits = np.array([ind.fitness.values for ind in pop])
     # print(fits)
-    average_pop = fits.mean()
-    print(average_pop)
+    #
+    # # print(fits)
+    # average_pop = fits.mean()
+    # print(average_pop)
     # if average_pop > best_pop[0]:
     #     best_pop = (pop)
